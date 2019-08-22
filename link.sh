9e3f7108828350bcf7f00d9f5abb783496ec6a43
Tobias Becker <nullpointeracception@gmail.com>
2019-08-22 08:27:05 +0200

    Next time, check out in home.

--- link.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-DIR=$(dirname "$(readlink -f $0)")
-cd "$DIR"
-find . -not -path './.git/*' -not -name .git -type d -exec mkdir -pv ~/{} \;
-find . -not -path './.git/*' -type f -not -regex ".*\.?$(basename $0).*" -not -name '*keyboard*' -exec ln $1 -sv $DIR/{} ~/{} \;
