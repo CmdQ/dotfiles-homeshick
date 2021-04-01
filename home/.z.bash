92ffc6b966a6a7cfc024c14716b5999aeb05e77e
Tobias Becker <tobias.becker@methodpark.de>
2021-04-01 09:16:51 +0200

    Fix shellcheck warnings

--- home/.z.bash
+++ home/.z.bash
@@ -1,16 +1,18 @@
+#!/usr/bin/env bash
+
 # Or do we want a more potent alternative?
 # https://github.com/deadc0de6/dotdrop
+
 if [[ -r "$HOME/.homesick/repos/homeshick/homeshick.sh" ]]; then
     source "$HOME/.homesick/repos/homeshick/homeshick.sh"
 else
     echo Install homeshick with
-    echo git clone git://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
+    echo git clone git://github.com/andsens/homeshick.git "$HOME/.homesick/repos/homeshick"
 fi
 
 if [[ -n $WSL_INTEROP ]]; then
-	#DISPLAY=$(grep nameserver /etc/resolv.conf | sed 's/.* //'):0.0
-    local nic_wsl="vEthernet (WSL)"
-    local nic_hyperv="vEthernet (Default Switch)"
+    nic_wsl="vEthernet (WSL)"
+    nic_hyperv="vEthernet (Default Switch)"
     if netsh.exe interface ip show interfaces | grep -qF "$nic_wsl"; then
 	    DISPLAY=$(netsh.exe interface ip show address "$nic_wsl" | sed -nE 's/^\s+IP[^:]+:\s+([0-9.]+)\s+/\1/p'):0.0
     elif netsh.exe interface ip show interfaces | grep -qF "$nic_hyperv"; then
@@ -30,5 +32,5 @@ mkcd() {
 }
 
 rmzi() {
-    rm -f *:Zone.Identifier
+    rm -f ./*:Zone.Identifier
 }
