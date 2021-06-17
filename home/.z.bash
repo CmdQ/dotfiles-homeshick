7a75c5d108c470bac5d9e7af044dd317feaa3b64
Tobias Becker <tobias.becker@methodpark.de>
2021-06-17 11:15:03 +0200

    Also check that poetry is eligible

--- home/.z.bash
+++ home/.z.bash
@@ -43,7 +43,7 @@ rmzi() {
 
 entr-py() {
     local runcmd="iblack ."
-    if command -v poetry &>/dev/null && ! { command -v pyenv &>/dev/null && pyenv virtualenv-prefix &>/dev/null ;} && [[ -z ${VIRTUAL_ENV} && -z ${POETRY_ACTIVE} ]]; then
+    if command -v poetry &>/dev/null && poetry env info &>/dev/null && ! { command -v pyenv &>/dev/null && pyenv virtualenv-prefix &>/dev/null ;} && [[ -z ${VIRTUAL_ENV} && -z ${POETRY_ACTIVE} ]]; then
         runcmd="poetry run $runcmd"
     fi
     false || while (( $? == 1 || $? == 2 )); do fdfind -e py | entr -ds "$runcmd"; done
